caff6ac0af691b3c3103eba4d7c6524ea5705685
Daniel Kienböck <dgk1@kabsi.at>
2019-09-17 22:20:43 +0200

    show notification with images and actions

--- app.js
+++ app.js
@@ -1,50 +1,116 @@
 // https://web-push-book.gauntface.com/demos/notification-examples/
+// https://whatwebcando.today/local-notifications.html
 
 var registeredServiceWorker = null;
+var subscription = null;
+const logContainer = document.getElementById('logContainer');
+const messageContent = document.getElementById('messageContent');
+
+function writeLog(message) {
+    console.log(message);
+    if(!logContainer.innerHTML) {
+        logContainer.innerHTML = "";
+    }
+    logContainer.innerHTML = logContainer.innerHTML + '<br />\n' + message + '<br/><hr/>'; 
+}
+
+// urlB64ToUint8Array is a magic function that will encode the base64 public key
+// to Array buffer which is needed by the subscription option
+const urlB64ToUint8Array = base64String => {
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
+    const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/')
+    const rawData = atob(base64)
+    const outputArray = new Uint8Array(rawData.length)
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i)
+    }
+    return outputArray
+  }
 
 function showSimpleMessage() {
-    registeredServiceWorker.showNotification('H3ll0 W0r1d', {
-        body: `var express = require('express'); var app = express();
-        app.get('/', function (req, res) {
-          res.send('Hello World!');
-        });`
-      });
 
+    if(Notification.maxActions == 2) {
+        // no arrrr
+    }
+
+    registeredServiceWorker.showNotification('Arrrrrrrrrr', { 
+        body: `Is this alrrrrrrrrright?`,
+        actions: [{
+            action: 'yes',
+            title: 'absolutely',
+            icon: '/yes.png'
+        },{
+            action: 'no',
+            title: 'nope',
+            icon: '/no.png'
+        }/*,{
+            action: 'maybe',
+            title: 'arrr',
+            icon: '/icon.png'
+        }*/],
+        requireInteraction: true,
+        icon: '/icon.png' 
+    });
+}
+
+function sendNotification() {
+    fetch('http://localhost:3000/sendNotification?message=' + messageContent.value, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(subscription),
+    })
 }
 
 window.addEventListener('load', function() {
 
     if (!('serviceWorker' in navigator)) {
-      console.log(`Service Worker isn't supported on this browser, disable or hide UI.`);
+      writeLog(`Service Worker isn't supported on this browser, disable or hide UI.`);
       return;
+    }
+    
+    if (!('PushManager' in window)) {
+        writeLog(`Push isn't supported on this browser, disable or hide UI.`);
+        return;
     }
 
     navigator.serviceWorker.addEventListener('message', function(event) {
-        console.log('Received a message from service worker: ', event.data);
+        writeLog('Received a message from service worker: ' + JSON.stringify(event.data));
     });
 
     navigator.serviceWorker.register('service-worker.js')
         .then(function(registration) {
-            console.log('Service worker successfully registered.');
+            writeLog('Service worker successfully registered.');
             registeredServiceWorker = registration;
+
+            const applicationServerKey = urlB64ToUint8Array(
+                'BAxsqBoOvwZjcoFmdMybEr2MgwsyBkRMEWjylFB8XnhNeCuxPBWNe8XpY-KlhB0G2pheUz7mzJswz6riSr59X-Q'
+            );
+            
+            (async () => {
+                const options = { applicationServerKey, userVisibleOnly: true };
+                subscription = await registeredServiceWorker.pushManager.subscribe(options);
+                writeLog(JSON.stringify(subscription))
+                await fetch('http://localhost:3000/register', {
+                    method: 'post',
+                    headers: {
+                    'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(subscription),
+                });
+            }) ();
         })
         .catch(function(err) {
-            console.error('Unable to register service worker.', err);
+            writeLog('ERROR: Unable to register service worker. "' + err + '"');
         });
 
-
-    if (!('PushManager' in window)) {
-      console.log(`Push isn't supported on this browser, disable or hide UI.`);
-      return;
-    }
-
     Notification.requestPermission((result) => {
-        // chrome://settings/content/notifications
+        writeLog('check <a href="chrome://settings/content/notifications">settings</a>');
         if (result === 'granted') {
-            console.log('notification allowed', result);
+            writeLog('notification allowed', result);
         } else {
-            console.log('notification not allowed', result);
+            writeLog('notification not allowed', result);
         }
     });
-
 });
--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // https://web-push-book.gauntface.com/demos/notification-examples/
 
-function sendToWindow(data) {
+function sendToWindow(data, event) {
     clients.matchAll({
       type: 'window',
       includeUncontrolled: true
@@ -27,25 +27,29 @@ function sendToWindow(data) {
   }
 
 self.addEventListener('push', function (event) {
-    console.log("service-worker", event, event.data, event.data.text());
-    sendToWindow(event.data);
+    console.log("service-worker:push", event, event.data, event.data.text());
+    sendToWindow(event.data.text(), event);
+
+    self.registration.showNotification('push from server', {
+      body: event.data.text()
+    });
 });
 
 self.addEventListener('notificationclick', function (event) {
-    console.log("service-worker", event, event.action, event.notification, event.notification.tag);
-    sendToWindow({tag:event.notification.tag, action: event.action}); // click if both empty
+    console.log("service-worker:notificationClick", event, event.action, event.notification, event.notification.tag);
+    sendToWindow({tag:event.notification.tag, action: event.action}, event); // click if both empty
     event.notification.close();
 });
 
 self.addEventListener('notificationclose', function (event) {
-    console.log("service-worker", event, event.notification);
-    sendToWindow("notificationclose");
+    console.log("service-worker:notificationClose", event, event.notification);
+    sendToWindow("notificationclose", event);
 });
 
-self.addEventListener('message', function (event) {
-    console.log("service-worker", 'Received message from page.', event, event.data);
-    self.dispatchEvent(new PushEvent('push', {
-        data: event.data
-    }));
-    sendToWindow(event.data);
-});
+// self.addEventListener('message', function (event) {
+//     console.log("service-worker:message", 'Received message from page.', event, event.data);
+//     self.dispatchEvent(new PushEvent('push', {
+//         data: event.data
+//     }));
+//     sendToWindow(event.data, event);
+// });
